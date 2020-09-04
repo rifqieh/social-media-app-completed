0304ccb0dbf39183f335714be5cf17b4b62e8ab3
Rifqi Eka Hardianto <ekarifqi1@gmail.com>
2020-09-04 22:37:13 +0700

    add constant.dart

---
+++ lib/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+var kWhiteColor = Colors.white;
+var kBlackColor = Colors.black;
+var bold = FontWeight.bold;
+var kBlueColor = Color(0xff035AA6);
+var kFacebookColor = Color(0xff3B5998);
